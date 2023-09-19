66ab6f05e20ee83154a15398dfc658f0a76932cc
mahfuzmunna <mahfuzmunna17@proton.me>
2023-09-19 19:31:04 +0600

    from dev_android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,22 +51,49 @@ android {
 
 dependencies {
 
-    implementation(Deps.coreKtx)
-    implementation(Deps.runtimeKtx)
-    implementation(Deps.activityCompose)
-    implementation(platform(Deps.composeBom))
-    implementation(Deps.composeUi)
-    implementation(Deps.uiGraphics)
-    implementation(Deps.toolingPreview)
-    implementation(Deps.material3)
+    dependencies {
 
-    testImplementation(Deps.junit)
+        val composeBom = platform("androidx.compose:compose-bom:2023.08.00")
+        implementation(composeBom)
+        androidTestImplementation(composeBom)
 
-    androidTestImplementation(Deps.junitExt)
-    androidTestImplementation(Deps.expressoCore)
-    androidTestImplementation(platform(Deps.composeBom))
-    androidTestImplementation(Deps.junit4)
+        // Choose one of the following:
+        // Material Design 3
+        implementation("androidx.compose.material3:material3")
+        // or Material Design 2
+        implementation("androidx.compose.material:material")
+        // or skip Material Design and build directly on top of foundational components
+        implementation("androidx.compose.foundation:foundation")
+        // or only import the main APIs for the underlying toolkit systems,
+        // such as input and measurement/layout
+        implementation("androidx.compose.ui:ui")
 
-    debugImplementation(Deps.uiTooling)
-    debugImplementation(Deps.testManifest)
+        // Android Studio Preview support
+        implementation("androidx.compose.ui:ui-tooling-preview")
+        debugImplementation("androidx.compose.ui:ui-tooling")
+
+        // UI Tests
+        androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+        debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+        // Optional - Included automatically by material, only add when you need
+        // the icons but not the material library (e.g. when using Material3 or a
+        // custom design system based on Foundation)
+        implementation("androidx.compose.material:material-icons-core")
+        // Optional - Add full set of material icons
+        implementation("androidx.compose.material:material-icons-extended")
+        // Optional - Add window size utils
+        implementation("androidx.compose.material3:material3-window-size-class")
+
+        // Optional - Integration with activities
+        implementation("androidx.activity:activity-compose:1.7.2")
+        // Optional - Integration with ViewModels
+        implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+        // Optional - Integration with LiveData
+        implementation("androidx.compose.runtime:runtime-livedata")
+        // Optional - Integration with RxJava
+        implementation("androidx.compose.runtime:runtime-rxjava2")
+
+    }
+
 }
